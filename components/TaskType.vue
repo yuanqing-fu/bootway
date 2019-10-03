35a0f7de4c463ed879db3ec14b12ec40568791c5
kofbossyagami <kofbossyagami@163.com>
2019-10-03 10:38:38 +0800

    Make frontend show correct task list from backend.

---
+++ components/TaskType.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="task-type-unit">
+    <div class="task-type-bar">重要</div>
+    <div class="day-task-view">
+      <div class="day-task">
+        <div class="day-task-inner">task 1</div>
+      </div>
+      <div class="day-task">
+        <div class="day-task-inner">task 2</div>
+      </div>
+      <div class="day-task">
+        <div class="day-task-inner">task 3</div>
+      </div>
+      <div class="day-task">
+        <div class="day-task-inner">task 4</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskType'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,7 @@
   <div class="day-page">
     <div class="header">Header</div>
     <div class="day-task-container">
-      <div class="task-type-unit">
-        <div class="task-type-bar">重要</div>
-        <div class="day-task-view">
-          <div class="day-task"><div class="day-task-inner">task 1</div></div>
-          <div class="day-task"><div class="day-task-inner">task 2</div></div>
-          <div class="day-task"><div class="day-task-inner">task 3</div></div>
-          <div class="day-task"><div class="day-task-inner">task 4</div></div>
-        </div>
-      </div>
-
-      <div class="task-type-unit">
-        <div class="task-type-bar">有趣</div>
-        <div class="day-task-view">
-          <div class="day-task"><div class="day-task-inner">task 1</div></div>
-          <div class="day-task"><div class="day-task-inner">task 2</div></div>
-        </div>
-      </div>
+      <task-type v-for="task in taskList" :key="task.task_id"></task-type>
     </div>
     <div class="footer">
       <div class="fun-area"></div>
@@ -28,86 +12,22 @@
 </template>
 
 <script>
-// import Logo from '~/components/Logo.vue'
+import TaskType from '~/components/TaskType.vue'
 
 export default {
-  // components: {
-  //
-  // }
+  components: {
+    TaskType
+  },
+  data() {
+    return {
+      // tasks: [1, 2, 3]
+    }
+  },
+  async asyncData({ $axios }) {
+    const response = await $axios.get(`http://api.test.com/tasks`)
+    // eslint-disable-next-line no-console
+    console.log(response.data)
+    return { taskList: response.data }
+  }
 }
 </script>
-
-<style>
-:root {
-  --leftbar-size: 30px;
-}
-
-body {
-  overflow: hidden;
-}
-
-.day-page {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.header {
-  flex: 0 0 auto;
-  background-color: #7f828b;
-  height: 44px;
-}
-
-.day-task-container {
-  background-color: orange;
-  flex: 1 1 auto;
-  position: relative;
-  overflow-y: auto;
-  display: flex;
-  flex-direction: column;
-}
-
-.day-task-container .task-type-unit {
-  flex: 0 0 auto;
-  display: flex;
-  flex-direction: row;
-}
-
-.day-task-container .task-type-unit .task-type-bar {
-  flex: 0 0 var(--leftbar-size);
-  background-color: blueviolet;
-}
-
-.day-task-container .task-type-unit .day-task-view {
-  flex: 1 0 auto;
-  background-color: green;
-  display: flex;
-  flex-direction: column;
-}
-
-.day-task-container .task-type-unit .day-task {
-  flex: 0 0 30px;
-  position: relative;
-}
-
-.day-task-container .task-type-unit .day-task .day-task-inner {
-  position: absolute;
-  top: 0;
-  left: 100px;
-}
-
-.footer {
-  flex: 0 0 auto;
-  display: flex;
-  flex-direction: row;
-}
-
-.footer .fun-area {
-  flex: 0 0 var(--leftbar-size);
-}
-
-.footer .day-time-bar {
-  flex: 1 0 auto;
-  background-color: #3b8070;
-}
-</style>
