a608c65050d88f0483b3e88f8e9c11a33dbc52b5
kofbossyagami <kofbossyagami@163.com>
2019-11-07 16:17:53 +0800

    Layout.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="footer">
-    <div class="footer-inner"></div>
+    <div class="footer-inner">
+      <div class="info-a">
+        <a
+          class="email"
+          title="kofbossyagami@163.com"
+          href="mailto:kofbossyagami@163.com"
+          >&#9993;</a
+        >
+        <nuxt-link class="" to="/">
+          关于我
+        </nuxt-link>
+      </div>
+      <div class="copyright">
+        &copy; 2019 Bootway.com 版权所有
+      </div>
+      <div class="info-b">
+        <a href="http://www.beian.miit.gov.cn/">蜀ICP备19032658号</a>
+      </div>
+    </div>
   </div>
 </template>
 <style>
 .footer {
+  padding: 0 10px;
   flex: 0 0 40px;
   display: flex;
   flex-direction: row;
@@ -12,6 +31,7 @@
   color: white;
   justify-content: center;
   align-items: center;
+  font-size: 12px;
 }
 
 .footer .footer-inner {
@@ -22,5 +42,38 @@
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
+  justify-content: space-between;
+}
+
+.footer a {
+  color: white;
+  font-size: 12px;
+}
+
+.footer a:hover {
+  filter: brightness(90%);
+}
+
+.footer .info-a {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer .info-a a {
+  margin-right: 10px;
+}
+
+.footer .info-a a.email {
+  font-size: 18px;
+}
+
+.footer .info-b a {
+  color: gray;
+}
+
+.footer .copyright {
+  color: #ccc;
 }
 </style>
