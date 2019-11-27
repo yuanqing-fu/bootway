38a41479ceffcce3d3382fd4e93479f78fbfca6c
kofbossyagami <kofbossyagami@163.com>
2019-11-27 16:43:27 +0800

    Bug fixed.

--- components/TaskUnit.vue
+++ components/TaskUnit.vue
@@ -73,6 +73,7 @@ export default {
       if (this.isMobile) {
         event.preventDefault()
         event.stopPropagation()
+        event.stopImmediatePropagation()
       }
     },
     handleEditTaskTextareaInputEvent() {
